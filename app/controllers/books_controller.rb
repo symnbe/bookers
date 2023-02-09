0d742782f47b819fe54216eb16cfd3eb6c835bd1
symnbe <ookaminingen@me.com>
2023-02-09 18:33:48 +0000

    [Remove]createggggggg

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-    
+
   end
 
   def create
     book = Book.new(book_params)
-    book.save
+    if book.save
     flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
+  else
+    render action: :index
+  end
   end
 
   def show
